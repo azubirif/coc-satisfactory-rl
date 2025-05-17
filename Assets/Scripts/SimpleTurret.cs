3816d15dacc3967dbdb6fbc669e3ffc5dab62163
Alejandro Zubiri <alejandrozubirifunes@gmail.com>
2025-05-17 17:38:46 +0200

    bullet

--- Assets/Scripts/SimpleTurret.cs
+++ Assets/Scripts/SimpleTurret.cs
@@ -4,12 +4,18 @@ using System.Linq;
 
 public class SimpleTurret : MonoBehaviour
 {
+    public float bulletDMG = 5f;
+    public float bulletSpeed = 20f;
+    public GameObject bulletPrefab;
     public bool drawRange = false;
     public float detectionRange = 5f;
     public LayerMask enemyMask;
     public float shootCooldown = 1f;
     private float currentShootCooldown = 0f;
     public float rotationSpeed = 2f;
+    public float shootDelay = 0.3f;
+    public Transform cannonTrans;
+    private float currentShootDelay = 0f;
 
     public bool isActive = true;
     void Start()
@@ -37,6 +43,13 @@ public class SimpleTurret : MonoBehaviour
         Collider2D[] enemiesDetected = Physics2D.OverlapCircleAll(transform.position, detectionRange, enemyMask);
         if (enemiesDetected.Length == 0) return;
         Transform closestEnemy = GetClosestEnemy(enemiesDetected);
+        currentShootDelay -= Time.deltaTime;
+
+        if (currentShootDelay <= 0f)
+        {
+            ShootTarget(closestEnemy);
+        }
+
         RotateToTarget(closestEnemy.transform);
 
         if (currentShootCooldown <= 0f)
@@ -48,7 +61,13 @@ public class SimpleTurret : MonoBehaviour
 
     private void ShootTarget(Transform target)
     {
-
+        currentShootDelay = shootCooldown;
+        GameObject shotBullet = Instantiate(bulletPrefab);
+        Vector2 direction = (target.position - transform.position).normalized;
+        float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg - 90f;
+        shotBullet.transform.position = cannonTrans.position;
+        shotBullet.transform.eulerAngles = new Vector3(0, 0, angle);
+        shotBullet.GetComponent<Rigidbody2D>().linearVelocity = direction * bulletSpeed;
     }
 
     private Transform GetClosestEnemy(Collider2D[] enemies)
